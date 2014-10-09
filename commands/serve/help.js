ab56c01e3be29d8ce53091dadf60012c9a1bfc02
Bart Riemens <briemens@crafity.com>
2014-10-09 11:01:39 +0200

    Added new switches (gzip, errors, headers, body) to crafity serve

--- commands/serve/help.js
+++ commands/serve/help.js
@@ -10,6 +10,26 @@ module.exports = new Help({
       default: true,
       description: "Specify port number (default is 2020)"
     },
+    "--gzip=<boolean>": {
+      aliases: [ "-g" ],
+      default: true,
+      description: "Turn gzip on or off"
+    },
+    "--headers": {
+      aliases: [ "-H" ],
+      default: false,
+      description: "Print headers"
+    },
+    "--body": {
+      aliases: [ "-b" ],
+      default: false,
+      description: "Print body"
+    },
+    "--errors=<percentage>": {
+      aliases: [ "-e" ],
+      default: 10,
+      description: "Throw random errors x percentage of the http posts"
+    },
     "--show": {
       aliases: [ "-s" ],
       default: false,
--- commands/serve/serve.js
+++ commands/serve/serve.js
@@ -6,19 +6,30 @@ module.exports = function ServeCommand(args, commands) {
 
   this.execute = function execute() {
     var port;
+    var gzip;
+    var showHeader = false;
+    var showBody = false;
+    var errorPercentage = 0;
 
     // Show the help overview in case something is missing or not correct
     if (args.hasArg('help', '--help', '-h')) {
       return help.overview();
     }
 
-    // If not specified which type of project, then default to --static
     if (!args.hasArg('-p', '--port')) {
       args.append("--port").append("2020");
     }
 
-    port = args.getArgValue('-p', '--port') || 2020;
-
+    if (!args.hasArg('-g', '--gzip')) {
+      args.append("--gzip");
+    }
+    
+    port = args.getArgValue('-p', '--port') === "true" ? 2020 : args.getArgValue('-p', '--port');
+    errorPercentage = args.getArgValue('-e', '--errors') === "true" ? 0 : args.getArgValue('-e', '--errors');
+    gzip = (args.getArgValue('-g', '--gzip') ? (args.getArgValue('-g', '--gzip')).toLowerCase() === "true" : true);
+    showHeader = args.getArgValue('-H', '--headers');
+    showBody = args.getArgValue('-b', '--body');
+    
     /*jslint node:true, white:true*/
     var http = require('http')
       , fs = require('fs')
@@ -30,8 +41,36 @@ module.exports = function ServeCommand(args, commands) {
       "use strict";
 
       try {
-        var url = "." + (req.url === "/" ? "/index.html" : req.url.split('?')[0])
-          , raw = fs.createReadStream(url);
+        var url = "." + (req.url === "/" ? "/index.html" : req.url.split('?')[0]);
+        var headers = {
+          'Content-Type': mime.lookup(url),
+          'Access-Control-Allow-Origin': '*'
+        };
+
+        console.log(req.method + " - " + url);
+        if (showHeader) {
+          console.log("HEADERS: ", req.headers);
+        }
+        if (showBody) {
+          req.on('data', function (data) {
+            console.log(data.toString());
+          });
+          req.on('close', function (data) {
+            console.log("\n");
+          });
+        }
+
+        if (req.method.toLowerCase() === "post") {
+          if (Math.floor(Math.random() * 100) > (100 - errorPercentage)) {
+            console.log("GENERATING ERROR");
+            res.writeHead(500, headers);
+            return res.end("Sorry!");
+          }
+          res.writeHead(200, headers);
+          return res.end("Thanks");
+        }
+
+        var raw = fs.createReadStream(url);
 
         raw.on('error', function (err) {
           console.log("arguments", arguments);
@@ -43,13 +82,15 @@ module.exports = function ServeCommand(args, commands) {
           return res.end(err.message + "\n" + err.stack);
         });
 
-        res.writeHead(200, {
-          'Content-Type': mime.lookup(url),
-          'content-encoding': 'gzip',
-          'Access-Control-Allow-Origin': '*'
-        });
-
-        return raw.pipe(zlib.createGzip()).pipe(res);
+        if (gzip) {
+          headers['content-encoding'] = 'gzip';
+        }
+        res.writeHead(200, headers);
+        if (gzip) {
+          return raw.pipe(zlib.createGzip()).pipe(res);
+        } else {
+          return raw.pipe(res);
+        }
 
       } catch (err) {
         console.log(err.message, err.stack);
